3a6e893a0a61128f8e615cab366ce089006d2de0
Nick Vasic <nick.vasic@certussolutions.com>
2021-01-19 01:03:02 +1100

    Changing the way media projection is invoked due to security changes in Android 10+ and the requirement for new API target level for Play Store

--- android/src/main/java/com/opentokreactnative/ScreenCaptureImageActivity.java
+++ android/src/main/java/com/opentokreactnative/ScreenCaptureImageActivity.java
@@ -13,6 +13,7 @@ import android.media.Image;
 import android.media.ImageReader;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -31,116 +32,10 @@ public class ScreenCaptureImageActivity extends Activity {
 
     private static final String TAG = ScreenCaptureImageActivity.class.getName();
     private static final int REQUEST_CODE = 100;
-    private static String STORE_DIRECTORY;
-    private static int IMAGES_PRODUCED;
-    private static final String SCREENCAP_NAME = "screencap";
-    private static final int VIRTUAL_DISPLAY_FLAGS = DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY | DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC;
-    private static MediaProjection sMediaProjection;
-
-    private MediaProjectionManager mProjectionManager;
-    private ImageReader mImageReader;
-    private Handler mHandler;
-    private Display mDisplay;
-    private VirtualDisplay mVirtualDisplay;
-    private int mDensity;
-    private int mWidth;
-    private int mHeight;
-    private int mRotation;
-    private OrientationChangeCallback mOrientationChangeCallback;
 
     public static ScreenCaptureImageActivity captureActivity;
     public static Bitmap latestBitmap;
 
-    private class ImageAvailableListener implements ImageReader.OnImageAvailableListener {
-        @Override
-        public void onImageAvailable(ImageReader reader) {
-            Image image = null;
-            FileOutputStream fos = null;
-            Bitmap bitmap = null;
-
-            try {
-                image = reader.acquireLatestImage();
-                if (image != null) {
-                    Image.Plane[] planes = image.getPlanes();
-                    ByteBuffer buffer = planes[0].getBuffer();
-                    int pixelStride = planes[0].getPixelStride();
-                    int rowStride = planes[0].getRowStride();
-                    int rowPadding = rowStride - pixelStride * mWidth;
-
-                    // create bitmap
-                    bitmap = Bitmap.createBitmap(mWidth + rowPadding / pixelStride, mHeight, Bitmap.Config.ARGB_8888);
-
-                    bitmap.copyPixelsFromBuffer(buffer);
-
-                    Bitmap realSizeBitmap = Bitmap.createBitmap(bitmap, 0, 0, mWidth, bitmap.getHeight());
-                    latestBitmap = realSizeBitmap;
-
-                    IMAGES_PRODUCED++;
-                    Log.e(TAG, "captured image: " + IMAGES_PRODUCED);
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (fos != null) {
-                    try {
-                        fos.close();
-                    } catch (IOException ioe) {
-                        ioe.printStackTrace();
-                    }
-                }
-
-                if (bitmap != null) {
-                    bitmap.recycle();
-                }
-
-                if (image != null) {
-                    image.close();
-                }
-            }
-        }
-    }
-
-    private class OrientationChangeCallback extends OrientationEventListener {
-
-        OrientationChangeCallback(Context context) {
-            super(context);
-        }
-
-        @Override
-        public void onOrientationChanged(int orientation) {
-            final int rotation = mDisplay.getRotation();
-            if (rotation != mRotation) {
-                mRotation = rotation;
-                try {
-                    // clean up
-                    if (mVirtualDisplay != null) mVirtualDisplay.release();
-                    if (mImageReader != null) mImageReader.setOnImageAvailableListener(null, null);
-
-                    // re-create virtual display depending on device width / height
-                    createVirtualDisplay();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private class MediaProjectionStopCallback extends MediaProjection.Callback {
-        @Override
-        public void onStop() {
-            Log.e("ScreenCapture", "stopping projection.");
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    if (mVirtualDisplay != null) mVirtualDisplay.release();
-                    if (mImageReader != null) mImageReader.setOnImageAvailableListener(null, null);
-                    if (mOrientationChangeCallback != null) mOrientationChangeCallback.disable();
-                    sMediaProjection.unregisterCallback(MediaProjectionStopCallback.this);
-                }
-            });
-        }
-    }
 
     /****************************************** Activity Lifecycle methods ************************/
     @Override
@@ -149,29 +44,25 @@ public class ScreenCaptureImageActivity extends Activity {
         this.captureActivity = this;
 
 
-        // call for the projection manager
-        mProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
-
         // start projection
         startProjection();
 
-        // start capture handling thread
-        new Thread() {
-            @Override
-            public void run() {
-                Looper.prepare();
-                mHandler = new Handler();
-                Looper.loop();
-            }
-        }.start();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE) {
-            sMediaProjection = mProjectionManager.getMediaProjection(resultCode, data);
 
-            if (sMediaProjection != null) {
+            // Need to start foreground service due to changes in API target 10+
+            Intent mediaProjectionIntent =  new Intent(this, ScreenCaptureMediaProjectionService.class);
+            mediaProjectionIntent.setAction(ScreenCaptureMediaProjectionService.ACTION_START);
+            mediaProjectionIntent.putExtra(ScreenCaptureMediaProjectionService.EXTRA_RESULT_DATA, data);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(mediaProjectionIntent);
+            }
+
+            /*if (sMediaProjection != null) {
                 File externalFilesDir = getExternalFilesDir(null);
                 if (externalFilesDir != null) {
                     STORE_DIRECTORY = externalFilesDir.getAbsolutePath() + "/screenshots/";
@@ -188,54 +79,29 @@ public class ScreenCaptureImageActivity extends Activity {
                     return;
                 }
 
-                // display metrics
-                DisplayMetrics metrics = getResources().getDisplayMetrics();
-                mDensity = metrics.densityDpi;
-                mDisplay = getWindowManager().getDefaultDisplay();
 
-                // create virtual display depending on device width / height
-                createVirtualDisplay();
-
-                // register orientation change callback
-                mOrientationChangeCallback = new OrientationChangeCallback(this);
-                if (mOrientationChangeCallback.canDetectOrientation()) {
-                    mOrientationChangeCallback.enable();
-                }
-
-                // register media projection stop callback
-                sMediaProjection.registerCallback(new MediaProjectionStopCallback(), mHandler);
-            }
+            }*/
         }
+        // Does this need to finish?
         this.finish();
     }
 
     /****************************************** UI Widget Callbacks *******************************/
     private void startProjection() {
-        startActivityForResult(mProjectionManager.createScreenCaptureIntent(), REQUEST_CODE);
+        MediaProjectionManager projectionManager = (MediaProjectionManager)
+                getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        startActivityForResult(projectionManager.createScreenCaptureIntent(), REQUEST_CODE);
     }
 
     public void stopProjection() {
-        mHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                if (sMediaProjection != null) {
-                    sMediaProjection.stop();
-                }
-            }
-        });
+        // Send stop action to service
+        Intent mediaProjectionIntent =  new Intent(this,
+                ScreenCaptureMediaProjectionService.class);
+        mediaProjectionIntent.setAction(ScreenCaptureMediaProjectionService.ACTION_STOP);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(mediaProjectionIntent);
+        }
     }
 
-    /****************************************** Factoring Virtual Display creation ****************/
-    private void createVirtualDisplay() {
-        // get width and height
-        Point size = new Point();
-        mDisplay.getRealSize(size);
-        mWidth = size.x;
-        mHeight = size.y;
-
-        // start capture reader
-        mImageReader = ImageReader.newInstance(mWidth, mHeight, PixelFormat.RGBA_8888, 2);
-        mVirtualDisplay = sMediaProjection.createVirtualDisplay(SCREENCAP_NAME, mWidth, mHeight, mDensity, VIRTUAL_DISPLAY_FLAGS, mImageReader.getSurface(), null, mHandler);
-        mImageReader.setOnImageAvailableListener(new ImageAvailableListener(), mHandler);
-    }
 }
